33fe9e1ca990f707d00062d343dd0b2a2fba46e7
bk75R <82215025+bk75R@users.noreply.github.com>
2022-02-03 21:41:40 +0000

    Create PHS_NASSS_Cardiac_WebPlotDigitizerGraphs.R
    Graphs all calls with JFMAM... format on dates and year labels with lines.

---
+++ PHS_NASSS_Cardiac_WebPlotDigitizerGraphs.R
@@ -0,0 +1,90 @@
+setwd(GraphsDirectory)
+####################
+
+########################################################
+# Graph WebPlotDigitizer data extracted from PHS graph #
+########################################################
+
+GraphSubtitle <- "@bouncingkitten\nhttps://www.drowningindata.blog"
+
+# Custom date formatting to get JFMAMJ... labels
+# http://www.jasonfabris.com/blog/dteformat/
+dte_formatter <- function(x) { 
+  #formatter for axis labels: J12, F12, M12, etc... 
+  mth <- substr(format(x, "%b"),1,1) 
+  mth 
+}
+
+# Create labels and positions for vertical lines which mark the start of each year
+
+vlines.cardiac.calls <- data.frame(Date = c(as.Date("2019-01-01"),as.Date("2020-01-01"),as.Date("2021-01-01"),as.Date("2022-01-01")),
+                                             Year = c(2019,2020,2021,2022), # This adds year labels to the lower faceted graph only
+                                             Type = rep("SevenDayAverage",4),
+                                             Calls = rep(220,4))
+cardiac.linetypes <- c("Baseline" = "dashed", 
+                      "SevenDayAverage" = "solid")
+cardiac.labels <- c("Baseline" = "Baseline",
+                      "SevenDayAverage" = "7 day average")
+cardiac.colours <- c("Baseline" = "grey50",
+                    "SevenDayAverage" = "black")
+
+# Graph cardiac calls (all years)
+cardiac.calls.graph <- ggplot(data = cardiac.calls,
+                              aes(x = Date,
+                                  y = Calls,
+                                  colour = Type,
+                                  linetype = Type)
+                              )+
+  ggtitle("Cardiac calls from the National Ambulance Syndromic Surveillance System: England ",
+          subtitle = GraphSubtitle)+
+  labs(caption = "Graph source: UK Health Security Agency/Public Health England")+
+  theme_tufte()+
+  theme(text = element_text(family=""),
+        plot.caption = element_text(hjust = 0),
+        panel.background = element_rect(fill = 'white', color = 'white'),
+        plot.background = element_rect(fill = 'white', color = 'white'),
+        legend.position="bottom")+
+  scale_x_date(name = "Date",
+               #limits = c(as.Date("2020-09-01"),as.Date("2021-12-31")),
+               breaks = "1 month",
+               labels = dte_formatter,
+               expand = c(0.03,0.03))+
+  scale_y_continuous(name = "Calls",
+                     labels = label_comma(accuracy = 1),
+                     limits = c(0,500),
+                     expand = c(0.06,0))+
+  geom_vline(data = vlines.cardiac.calls,
+             aes(xintercept = Date),
+             colour = "grey75",
+             show.legend = FALSE)+
+  geom_text(data = vlines.cardiac.calls,
+            aes(label = Year,
+                x = Date,
+                y = Calls),
+            nudge_x = 15,
+            nudge_y = 5,
+            colour="grey25",
+            angle = 90,
+            show.legend = FALSE)+
+  geom_line(show.legend = TRUE,
+            size = 0.65,
+            na.rm = TRUE)+
+  # geom_point(show.legend = FALSE)+
+  scale_linetype_manual(name = "Call type",
+                      values = cardiac.linetypes,
+                      labels = cardiac.labels)+
+  scale_colour_manual(name = "Call type",
+                        values = cardiac.colours,
+                        labels = cardiac.labels)
+
+ggsave(cardiac.calls.graph,filename = paste(GraphFileNameRoot,"_CardiacCalls.png",sep=""),
+       device = png,
+       #dpi = 320,
+       units = "px",
+       width = 2300,
+       height = 1600,
+       bg = "white"
+)
+
+####################
+setwd(RootDirectory)
