966f4069a22f211658bafab8cb9facd6fa1fead1
bk75R <82215025+bk75R@users.noreply.github.com>
2022-02-03 22:53:34 +0000

    Update PHS_NASSS_Cardiac_WebPlotDigitizerGraphs.R
    Added code to create two graphs: one with full y range (0 to 500) and one with reduced range to show detail (roughly 220 to 450). Removed some unnecessary comments.

--- PHS_NASSS_Cardiac_WebPlotDigitizerGraphs.R
+++ PHS_NASSS_Cardiac_WebPlotDigitizerGraphs.R
@@ -1,16 +1,16 @@
 setwd(GraphsDirectory)
 ####################
 
-########################################################
-# Graph WebPlotDigitizer data extracted from PHS graph #
-########################################################
+###########################################################
+# Graph WebPlotDigitizer data extracted from NASSS graphs #
+###########################################################
 
 GraphSubtitle <- "@bouncingkitten\nhttps://www.drowningindata.blog"
 
 # Custom date formatting to get JFMAMJ... labels
 # http://www.jasonfabris.com/blog/dteformat/
 dte_formatter <- function(x) { 
-  #formatter for axis labels: J12, F12, M12, etc... 
+  #formatter for axis labels: J, F, M, etc... 
   mth <- substr(format(x, "%b"),1,1) 
   mth 
 }
@@ -18,9 +18,9 @@ dte_formatter <- function(x) {
 # Create labels and positions for vertical lines which mark the start of each year
 
 vlines.cardiac.calls <- data.frame(Date = c(as.Date("2019-01-01"),as.Date("2020-01-01"),as.Date("2021-01-01"),as.Date("2022-01-01")),
-                                             Year = c(2019,2020,2021,2022), # This adds year labels to the lower faceted graph only
+                                             Year = c(2019,2020,2021,2022),
                                              Type = rep("SevenDayAverage",4),
-                                             Calls = rep(220,4))
+                                             Calls = rep(10,4))
 cardiac.linetypes <- c("Baseline" = "dashed", 
                       "SevenDayAverage" = "solid")
 cardiac.labels <- c("Baseline" = "Baseline",
@@ -29,7 +29,7 @@ cardiac.colours <- c("Baseline" = "grey50",
                     "SevenDayAverage" = "black")
 
 # Graph cardiac calls (all years)
-cardiac.calls.graph <- ggplot(data = cardiac.calls,
+cardiac.calls.graph.basic <- ggplot(data = cardiac.calls,
                               aes(x = Date,
                                   y = Calls,
                                   colour = Type,
@@ -45,7 +45,6 @@ cardiac.calls.graph <- ggplot(data = cardiac.calls,
         plot.background = element_rect(fill = 'white', color = 'white'),
         legend.position="bottom")+
   scale_x_date(name = "Date",
-               #limits = c(as.Date("2020-09-01"),as.Date("2021-12-31")),
                breaks = "1 month",
                labels = dte_formatter,
                expand = c(0.03,0.03))+
@@ -53,6 +52,17 @@ cardiac.calls.graph <- ggplot(data = cardiac.calls,
                      labels = label_comma(accuracy = 1),
                      limits = c(0,500),
                      expand = c(0.06,0))+
+  geom_line(show.legend = TRUE,
+            size = 0.65,
+            na.rm = TRUE)+
+  scale_linetype_manual(name = "Call type",
+                      values = cardiac.linetypes,
+                      labels = cardiac.labels)+
+  scale_colour_manual(name = "Call type",
+                        values = cardiac.colours,
+                        labels = cardiac.labels)
+
+cardiac.calls.graph.original <- cardiac.calls.graph.basic + 
   geom_vline(data = vlines.cardiac.calls,
              aes(xintercept = Date),
              colour = "grey75",
@@ -65,21 +75,43 @@ cardiac.calls.graph <- ggplot(data = cardiac.calls,
             nudge_y = 5,
             colour="grey25",
             angle = 90,
-            show.legend = FALSE)+
-  geom_line(show.legend = TRUE,
-            size = 0.65,
-            na.rm = TRUE)+
-  # geom_point(show.legend = FALSE)+
-  scale_linetype_manual(name = "Call type",
-                      values = cardiac.linetypes,
-                      labels = cardiac.labels)+
-  scale_colour_manual(name = "Call type",
-                        values = cardiac.colours,
-                        labels = cardiac.labels)
+            show.legend = FALSE)
 
-ggsave(cardiac.calls.graph,filename = paste(GraphFileNameRoot,"_CardiacCalls.png",sep=""),
+ggsave(cardiac.calls.graph.original,filename = paste(GraphFileNameRoot," NASSS Cardiac Calls (original y range).png",sep=""),
        device = png,
-       #dpi = 320,
+       units = "px",
+       width = 2300,
+       height = 1600,
+       bg = "white"
+)
+
+# Regraph with closer y axis to show more detail
+vlines.cardiac.calls.high <- data.frame(Date = c(as.Date("2019-01-01"),as.Date("2020-01-01"),as.Date("2021-01-01"),as.Date("2022-01-01")),
+                                   Year = c(2019,2020,2021,2022),
+                                   Type = rep("SevenDayAverage",4),
+                                   Calls = rep(220,4))
+
+cardiac.calls.graph.closer <- cardiac.calls.graph.basic + 
+  scale_y_continuous(name = "Calls",
+                     labels = label_comma(accuracy = 1),
+                     limits = c(NA,NA),
+                     expand = c(0.06,0))+
+  geom_vline(data = vlines.cardiac.calls.high,
+             aes(xintercept = Date),
+             colour = "grey75",
+             show.legend = FALSE)+
+  geom_text(data = vlines.cardiac.calls.high,
+            aes(label = Year,
+                x = Date,
+                y = Calls),
+            nudge_x = 15,
+            nudge_y = 5,
+            colour="grey25",
+            angle = 90,
+            show.legend = FALSE)
+  
+ggsave(cardiac.calls.graph.closer,filename = paste(GraphFileNameRoot," NASSS Cardiac Calls (reduced y range).png",sep=""),
+       device = png,
        units = "px",
        width = 2300,
        height = 1600,
