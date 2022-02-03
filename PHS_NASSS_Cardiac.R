ace66db35a63b790dc47d00c5849c0d4b740d9a6
bk75R <82215025+bk75R@users.noreply.github.com>
2022-02-03 16:56:44 +0000

    Added R files
    Copied from earlier work,

---
+++ PHS_NASSS_Cardiac.R
@@ -0,0 +1,41 @@
+#####################################################################################
+#
+#
+# PHS_NASSS_Cardiac.R
+# 
+# Graphing and cleaning up UK NASSS cardiac calls data
+# ============================================================================
+#
+#
+####################################################################################
+
+# Set up libraries
+library(ggplot2)
+library(ggthemes) # theme_tufte()
+library(tidyr)
+library(dplyr)
+library(httr)
+library(scales)
+library(lubridate)
+library(readxl)
+
+###################################################
+#                                                 #
+# Load and reformat data                          #
+#                                                 #
+###################################################
+
+# Set working directories
+RootDirectory <- getwd() # This should be the directory in which the .R file is found.
+DataDirectory <- paste(RootDirectory,"/data",sep = "")
+GraphsDirectory <- paste(RootDirectory,"/graphs",sep = "")
+WebPlotDigitizerDirectory <- paste(RootDirectory,"/WebPlotDigitizer",sep = "")
+
+GraphFileNameRoot <- as.character(format(Sys.Date(),"%Y-%m-%d"))
+
+source("PHS_NASSS_Cardiac_WebPlotDigitizerLoad.R") # Load data extracted from graph using WebPlotDigitizer - https://apps.automeris.io/wpd/
+
+source("PHS_NASSS_Cardiac_WebPlotDigitizerGraph.R") # Graph data from WebPlotDigitizer
+
+##########################################################################################
+setwd(RootDirectory)
