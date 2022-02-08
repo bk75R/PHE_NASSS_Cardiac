2a8f7eb91096ea72c205babd4c316f7fe58c47b2
bk75R <82215025+bk75R@users.noreply.github.com>
2022-02-08 22:15:10 +0000

    Update PHS_NASSS_Cardiac_WebPlotDigitizerSaveData.R
    Added output of summary totals for cardiac.2020 etc.

--- PHS_NASSS_Cardiac_WebPlotDigitizerSaveData.R
+++ PHS_NASSS_Cardiac_WebPlotDigitizerSaveData.R
@@ -15,6 +15,19 @@ cardiac.calls.spreadsheet <- rbind.data.frame(cardiac.2020,cardiac.2021,cardiac.
 write.csv(cardiac.calls.spreadsheet,file = "2019-2022 NASSS Cardiac calls (spreadsheet).csv",
           row.names = FALSE)
 
+# Output summary totals for each year.
+# Recall that cardiac.2020 is the dataset for the January 2020 report,
+# so cardiac.2020 contains the totals for 2019 (and so on for the other similar data frames)
+
+cardiac.2020 %>%
+  summarise(Daily = sum(SevenDayAverage,na.rm = TRUE),Baseline = sum(Baseline))
+
+cardiac.2021 %>%
+  summarise(Daily = sum(SevenDayAverage,na.rm = TRUE),Baseline = sum(Baseline))
+
+cardiac.2022 %>%
+  summarise(Daily = sum(SevenDayAverage,na.rm = TRUE),Baseline = sum(Baseline))
+
 # Remove redundant data frames
 rm(cardiac.2020,cardiac.2020.7DA,cardiac.2020.baseline,cardiac.2020.long,
    cardiac.2021,cardiac.2021.7DA,cardiac.2021.baseline,cardiac.2021.long,
