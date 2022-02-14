6b1eb0ae2801a03a2bd825253a521fd7b2236052
bk75R <82215025+bk75R@users.noreply.github.com>
2022-02-14 12:40:03 +0000

    Added in 2019 report data
    Added data extracted from 2019 report graphs. This fills the gap at the start of the 2020 report data.
    
    Still working on graph display.

--- PHS_NASSS_Cardiac_WebPlotDigitizerLoad.R
+++ PHS_NASSS_Cardiac_WebPlotDigitizerLoad.R
@@ -6,6 +6,44 @@ setwd(WebPlotDigitizerDirectory)
 ##########################################################
 
 #############
+# 2019 data #
+#############
+
+# Load data
+WPD.2019.7DA.filename <- "2019 Ambulance bulletin - cardiac - 7 day average.csv"
+WPD.2019.baseline.filename <- "2019 Ambulance bulletin - cardiac - baseline.csv"
+cardiac.2019.7DA <- read.csv(WPD.2019.7DA.filename,header = FALSE, sep = ",")
+cardiac.2019.baseline <- read.csv(WPD.2019.baseline.filename,header = FALSE, sep = ",")
+
+# Change column names
+column.names <- c("Date","Calls")
+colnames(cardiac.2019.7DA) <- column.names
+colnames(cardiac.2019.baseline) <- column.names
+
+# Convert date formats
+cardiac.2019.7DA$Date <- as_date(cardiac.2019.7DA$Date)
+cardiac.2019.baseline$Date <- as_date(cardiac.2019.baseline$Date)
+
+# Remove duplicates
+cardiac.2019.7DA <- cardiac.2019.7DA[!duplicated(cardiac.2019.7DA$Date), ]
+cardiac.2019.baseline <- cardiac.2019.baseline[!duplicated(cardiac.2019.baseline$Date), ]
+
+cardiac.2019 <- right_join(cardiac.2019.7DA,cardiac.2019.baseline,by = "Date")
+colnames.cardiac <- c("Date","SevenDayAverage","Baseline")
+colnames(cardiac.2019) <- colnames.cardiac
+cardiac.2019 <- drop_na(cardiac.2019) # Removes rows where there's only one value available.
+# This is due to the overlapping lines in the original plots.
+
+# Keep only January 2019 data
+#cardiac.2019 <- filter(cardiac.2019,Date >= as.Date("2019-01-01") & Date <= as.Date("2019-01-31"))
+
+# Pivot the datasets to make them longer
+cardiac.2019.long <- cardiac.2019 %>%
+  mutate(SevenDayAverage = na.spline(SevenDayAverage)) %>%
+  pivot_longer(SevenDayAverage:Baseline,names_to = "Type",values_to = "Calls")
+
+
+#############
 # 2020 data #
 #############
 
@@ -131,39 +169,40 @@ cardiac.2022.long <- cardiac.2022 %>%
 # Baseline data has different date range to seven day average data.
 
 # Calculate baseline approximation
-cardiac.2020_2022 <- rbind.data.frame(cardiac.2020,cardiac.2021,cardiac.2022)
-date.range.baseline <- seq(min(cardiac.2020_2022$Date),max(cardiac.2020_2022$Date),1)
+cardiac.2019_2022 <- rbind.data.frame(cardiac.2019,cardiac.2020,cardiac.2021,cardiac.2022)
+#date.range.baseline <- seq(as.Date("2019-01-01"),max(cardiac.2019_2022$Date),1) # Start sequence at 01/01/2019 because cardiac.2019 starts at 02/01/2019.
+date.range.baseline <- seq(min(cardiac.2019_2022$Date),max(cardiac.2019_2022$Date),1)
 #day.seq.baseline.full <- seq(1,length(date.range.baseline),1)
-cardiac.2020_2022.baseline.approx <- approx(cardiac.2020_2022$Date,
-                                            cardiac.2020_2022$Baseline,
+cardiac.2019_2022.baseline.approx <- approx(cardiac.2019_2022$Date,
+                                            cardiac.2019_2022$Baseline,
                                             method = "linear",
                                             n = length(date.range.baseline))
-cardiac.2020_2022.baseline.approx <- cbind.data.frame(date.range.baseline,cardiac.2020_2022.baseline.approx$y)
-colnames(cardiac.2020_2022.baseline.approx) <- c("Date","Baseline")
+cardiac.2019_2022.baseline.approx <- cbind.data.frame(date.range.baseline,cardiac.2019_2022.baseline.approx$y)
+colnames(cardiac.2019_2022.baseline.approx) <- c("Date","Baseline")
 
 # Calculate seven day average approximation (different date range)
-cardiac.2020_2022.sevendayaverage <- cbind.data.frame(cardiac.2020_2022$Date,
-                                                      cardiac.2020_2022$SevenDayAverage)
-colnames(cardiac.2020_2022.sevendayaverage) <- c("Date","SevenDayAverage")
-cardiac.2020_2022.sevendayaverage <- cardiac.2020_2022.sevendayaverage %>%
+cardiac.2019_2022.sevendayaverage <- cbind.data.frame(cardiac.2019_2022$Date,
+                                                      cardiac.2019_2022$SevenDayAverage)
+colnames(cardiac.2019_2022.sevendayaverage) <- c("Date","SevenDayAverage")
+cardiac.2019_2022.sevendayaverage <- cardiac.2019_2022.sevendayaverage %>%
   filter(!is.na(SevenDayAverage)) # Remove rows with NA values.
-date.range.sevendayaverage <- seq(min(cardiac.2020_2022.sevendayaverage$Date),max(cardiac.2020_2022.sevendayaverage$Date),1)
-cardiac.2020_2022.sevendayaverage.approx <- approx(cardiac.2020_2022.sevendayaverage$Date,
-                                                   cardiac.2020_2022.sevendayaverage$SevenDayAverage,
+date.range.sevendayaverage <- seq(min(cardiac.2019_2022.sevendayaverage$Date),max(cardiac.2019_2022.sevendayaverage$Date),1)
+cardiac.2019_2022.sevendayaverage.approx <- approx(cardiac.2019_2022.sevendayaverage$Date,
+                                                   cardiac.2019_2022.sevendayaverage$SevenDayAverage,
                                                    method = "linear",
                                                    n = length(date.range.sevendayaverage))
-cardiac.2020_2022.sevendayaverage.approx <- cbind.data.frame(date.range.sevendayaverage,cardiac.2020_2022.sevendayaverage.approx$y)
-colnames(cardiac.2020_2022.sevendayaverage.approx) <- c("Date","SevenDayAverage")
+cardiac.2019_2022.sevendayaverage.approx <- cbind.data.frame(date.range.sevendayaverage,cardiac.2019_2022.sevendayaverage.approx$y)
+colnames(cardiac.2019_2022.sevendayaverage.approx) <- c("Date","SevenDayAverage")
 
 
-cardiac.2020_2022.approx <- full_join(cardiac.2020_2022.baseline.approx,cardiac.2020_2022.sevendayaverage.approx)
+cardiac.2019_2022.approx <- full_join(cardiac.2019_2022.baseline.approx,cardiac.2019_2022.sevendayaverage.approx)
 
 #####################
 # Combine datasets  #
 #####################
 
-# cardiac.calls <- rbind.data.frame(cardiac.2020.long,cardiac.2021.long,cardiac.2022.long) # Non-interpolated data
-cardiac.calls <- cardiac.2020_2022.approx %>%
+# cardiac.calls <- rbind.data.frame(cardiac.2019.long,cardiac.2021.long,cardiac.2022.long) # Non-interpolated data
+cardiac.calls <- cardiac.2019_2022.approx %>%
   pivot_longer(SevenDayAverage:Baseline,names_to = "Type",values_to = "Calls")
 
 ####################
--- PHS_NASSS_Cardiac_WebPlotDigitizerSaveData.R
+++ PHS_NASSS_Cardiac_WebPlotDigitizerSaveData.R
@@ -18,22 +18,26 @@ write.csv(cardiac.2020_2022.approx,file = "2019-2022 NASSS Cardiac calls (spread
 # Recall that cardiac.2020.summary contains the dataset for the January 2020 report,
 # so cardiac.2020.summary contains the totals for 2019 (and so on for the other similar summary data)
 
-cardiac.2020.summary <- cardiac.2020_2022.approx %>%
-  filter(Date <= as.Date("2019-12-31")) %>%
+# cardiac.2019.summary <- cardiac.2019_2022.approx %>%
+#   filter(Date >= as.Date("2018-01-01") & Date <= as.Date("2018-12-31")) %>%
+#   summarise(Daily = sum(SevenDayAverage,na.rm = TRUE),Baseline = sum(Baseline))
+
+cardiac.2020.summary <- cardiac.2019_2022.approx %>%
+  filter(Date >= as.Date("2019-01-01") & Date <= as.Date("2019-12-31")) %>%
   summarise(Daily = sum(SevenDayAverage,na.rm = TRUE),Baseline = sum(Baseline))
 
-cardiac.2021.summary <- cardiac.2020_2022.approx %>%
+cardiac.2021.summary <- cardiac.2019_2022.approx %>%
   filter(Date >= as.Date("2020-01-01") & Date <= as.Date("2020-12-31")) %>%
   summarise(Daily = sum(SevenDayAverage,na.rm = TRUE),Baseline = sum(Baseline))
 
-cardiac.2022.summary <- cardiac.2020_2022.approx %>%
-  filter(Date >= as.Date("2021-01-01")) %>%
+cardiac.2022.summary <- cardiac.2019_2022.approx %>%
+  filter(Date >= as.Date("2021-01-01") & Date <= as.Date("2021-12-31")) %>%
   summarise(Daily = sum(SevenDayAverage,na.rm = TRUE),Baseline = sum(Baseline))
 
 # Remove redundant data frames
-rm(cardiac.2020,cardiac.2020.7DA,cardiac.2020.baseline,cardiac.2020.long,
-   cardiac.2021,cardiac.2021.7DA,cardiac.2021.baseline,cardiac.2021.long,
-   cardiac.2022,cardiac.2022.7DA,cardiac.2022.baseline,cardiac.2022.long)
+# rm(cardiac.2020,cardiac.2020.7DA,cardiac.2020.baseline,cardiac.2020.long,
+#    cardiac.2021,cardiac.2021.7DA,cardiac.2021.baseline,cardiac.2021.long,
+#    cardiac.2022,cardiac.2022.7DA,cardiac.2022.baseline,cardiac.2022.long)
 
 ####################
 setwd(RootDirectory)
