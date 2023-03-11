9c7574dc367d9881cfb6bd10f4eaa4c28dc44210
bk75R <82215025+bk75R@users.noreply.github.com>
2023-03-11 22:25:36 +0000

    Update PHS_NASSS_Cardiac_WebPlotDigitizerLoad.R
    Added data read and reformatting/filtering for 2023 data too.

--- PHS_NASSS_Cardiac_WebPlotDigitizerLoad.R
+++ PHS_NASSS_Cardiac_WebPlotDigitizerLoad.R
@@ -203,6 +203,46 @@ cardiac.2022.rest <- cardiac.2022 %>%
 
 cardiac.2022 <- rbind.data.frame(cardiac.2022.rest,cardiac.2022.end)
 
+
+#############
+# 2023 data #
+#############
+
+# Load data
+WPD.2023.7DA.filename <- "2023 week 08 Ambulance bulletin - cardiac - 7 day average.csv"
+WPD.2023.baseline.filename <- "2023 week 08 Ambulance bulletin - cardiac - baseline.csv"
+cardiac.2023.7DA <- read.csv(WPD.2023.7DA.filename,header = FALSE, sep = ",")
+cardiac.2023.baseline <- read.csv(WPD.2023.baseline.filename,header = FALSE, sep = ",")
+
+# Change column names
+column.names <- c("Date","Calls")
+colnames(cardiac.2023.7DA) <- column.names
+colnames(cardiac.2023.baseline) <- column.names
+
+# Convert date formats
+cardiac.2023.7DA$Date <- as_date(cardiac.2023.7DA$Date)
+cardiac.2023.baseline$Date <- as_date(cardiac.2023.baseline$Date)
+
+# Remove duplicates
+cardiac.2023.7DA <- cardiac.2023.7DA[!duplicated(cardiac.2023.7DA$Date), ]
+cardiac.2023.baseline <- cardiac.2023.baseline[!duplicated(cardiac.2023.baseline$Date), ]
+
+cardiac.2023 <- full_join(cardiac.2023.7DA,cardiac.2023.baseline,by = "Date")
+colnames.cardiac <- c("Date","SevenDayAverage","Baseline")
+colnames(cardiac.2023) <- colnames.cardiac
+
+# Split off end portion of data as baseline data extends beyond 2023 seven day average data.
+cardiac.2023.end <- filter(cardiac.2023,Date >= as.Date("2023-01-20"))
+cardiac.2023.rest <- cardiac.2023 %>%
+  filter(Date < as.Date("2023-01-20")) %>%
+  arrange(Date)
+
+# cardiac.2023.SevenDayAverage.filled <- na.spline(cardiac.2023.rest$SevenDayAverage)
+# cardiac.2023.rest$SevenDayAverage <- cardiac.2023.SevenDayAverage.filled
+
+cardiac.2023 <- rbind.data.frame(cardiac.2023.rest,cardiac.2023.end)
+
+
 ######################################################
 # Filter out duplicated date ranges from each report #
 ######################################################
@@ -258,54 +298,62 @@ cardiac.2022.long <- cardiac.2022 %>%
   pivot_longer(SevenDayAverage:Baseline,names_to = "Type",values_to = "Calls") %>%
   mutate(Report = "2022wk03")
 
+
+cardiac.2023.long <- cardiac.2023 %>%
+  # mutate(SevenDayAverage = na.spline(SevenDayAverage)) %>%
+  # mutate(Baseline = na.spline(Baseline)) %>%
+  pivot_longer(SevenDayAverage:Baseline,names_to = "Type",values_to = "Calls") %>%
+  mutate(Report = "2023wk08")
+
 ##################################################################################
 # Interpolate (approx, linear) between points in baseline and 7 day average data #
 # This will yield modelled daily data for each data type.                        #
 ##################################################################################
 
 # Calculate baseline approximation
-cardiac.2019_2022 <- rbind.data.frame(cardiac.2019.filter,
+cardiac.2019_2023 <- rbind.data.frame(cardiac.2019.filter,
                                       cardiac.2020,
                                       cardiac.2020wk10.filter,
                                       cardiac.2021,
                                       cardiac.2021wk08.filter,
-                                      cardiac.2022)
+                                      cardiac.2022,
+                                      cardiac.2023)
 # Remove duplicates
-cardiac.2019_2022 <- cardiac.2019_2022[!duplicated(cardiac.2019_2022$Date), ]
+cardiac.2019_2023 <- cardiac.2019_2023[!duplicated(cardiac.2019_2023$Date), ]
 
-date.range.baseline <- seq(min(cardiac.2019$Date),max(cardiac.2019_2022$Date),1) # Start sequence at 01/01/2019 because cardiac.2019 starts at 02/01/2019.
-# date.range.baseline <- seq(min(cardiac.2019_2022$Date),max(cardiac.2019_2022$Date),1)
+date.range.baseline <- seq(min(cardiac.2019$Date),max(cardiac.2019_2023$Date),1) # Start sequence at 01/01/2019 because cardiac.2019 starts at 02/01/2019.
+# date.range.baseline <- seq(min(cardiac.2019_2023$Date),max(cardiac.2019_2023$Date),1)
 #day.seq.baseline.full <- seq(1,length(date.range.baseline),1)
-cardiac.2019_2022.baseline.approx <- approx(cardiac.2019_2022$Date,
-                                            cardiac.2019_2022$Baseline,
+cardiac.2019_2023.baseline.approx <- approx(cardiac.2019_2023$Date,
+                                            cardiac.2019_2023$Baseline,
                                             method = "linear",
                                             n = length(date.range.baseline))
-cardiac.2019_2022.baseline.approx <- cbind.data.frame(date.range.baseline,cardiac.2019_2022.baseline.approx$y)
-colnames(cardiac.2019_2022.baseline.approx) <- c("Date","Baseline")
+cardiac.2019_2023.baseline.approx <- cbind.data.frame(date.range.baseline,cardiac.2019_2023.baseline.approx$y)
+colnames(cardiac.2019_2023.baseline.approx) <- c("Date","Baseline")
 
 # Calculate seven day average approximation (different date range)
-cardiac.2019_2022.sevendayaverage <- cbind.data.frame(cardiac.2019_2022$Date,
-                                                      cardiac.2019_2022$SevenDayAverage)
-colnames(cardiac.2019_2022.sevendayaverage) <- c("Date","SevenDayAverage")
-cardiac.2019_2022.sevendayaverage <- cardiac.2019_2022.sevendayaverage %>%
+cardiac.2019_2023.sevendayaverage <- cbind.data.frame(cardiac.2019_2023$Date,
+                                                      cardiac.2019_2023$SevenDayAverage)
+colnames(cardiac.2019_2023.sevendayaverage) <- c("Date","SevenDayAverage")
+cardiac.2019_2023.sevendayaverage <- cardiac.2019_2023.sevendayaverage %>%
   filter(!is.na(SevenDayAverage)) # Remove rows with NA values.
-date.range.sevendayaverage <- seq(min(cardiac.2019_2022.sevendayaverage$Date),max(cardiac.2019_2022.sevendayaverage$Date),1)
-cardiac.2019_2022.sevendayaverage.approx <- approx(cardiac.2019_2022.sevendayaverage$Date,
-                                                   cardiac.2019_2022.sevendayaverage$SevenDayAverage,
+date.range.sevendayaverage <- seq(min(cardiac.2019_2023.sevendayaverage$Date),max(cardiac.2019_2023.sevendayaverage$Date),1)
+cardiac.2019_2023.sevendayaverage.approx <- approx(cardiac.2019_2023.sevendayaverage$Date,
+                                                   cardiac.2019_2023.sevendayaverage$SevenDayAverage,
                                                    method = "linear",
                                                    n = length(date.range.sevendayaverage))
-cardiac.2019_2022.sevendayaverage.approx <- cbind.data.frame(date.range.sevendayaverage,cardiac.2019_2022.sevendayaverage.approx$y)
-colnames(cardiac.2019_2022.sevendayaverage.approx) <- c("Date","SevenDayAverage")
+cardiac.2019_2023.sevendayaverage.approx <- cbind.data.frame(date.range.sevendayaverage,cardiac.2019_2023.sevendayaverage.approx$y)
+colnames(cardiac.2019_2023.sevendayaverage.approx) <- c("Date","SevenDayAverage")
 
 
-cardiac.2019_2022.approx <- full_join(cardiac.2019_2022.baseline.approx,cardiac.2019_2022.sevendayaverage.approx)
+cardiac.2019_2023.approx <- full_join(cardiac.2019_2023.baseline.approx,cardiac.2019_2023.sevendayaverage.approx)
 
 #####################
 # Combine datasets  #
 #####################
 
 # cardiac.calls <- rbind.data.frame(cardiac.2019.long,cardiac.2021.long,cardiac.2022.long) # Non-interpolated data
-cardiac.calls <- cardiac.2019_2022.approx %>%
+cardiac.calls <- cardiac.2019_2023.approx %>%
   pivot_longer(SevenDayAverage:Baseline,names_to = "Type",values_to = "Calls")
 
 cardiac.calls.report.graphs <- rbind.data.frame(cardiac.2019.long,
@@ -313,7 +361,8 @@ cardiac.calls.report.graphs <- rbind.data.frame(cardiac.2019.long,
                                                 cardiac.2020wk10.long,
                                                 cardiac.2021.long,
                                                 cardiac.2021wk08.long,
-                                                cardiac.2022.long)
+                                                cardiac.2022.long,
+                                                cardiac.2023.long)
 
 cardiac.calls.report.graphs <- drop_na(cardiac.calls.report.graphs) # Remove all NA values.
 ####################
