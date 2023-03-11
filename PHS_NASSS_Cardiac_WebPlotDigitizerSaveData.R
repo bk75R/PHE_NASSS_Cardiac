f02ada0544b015a99bab08797dcec9be3a483f7c
bk75R <82215025+bk75R@users.noreply.github.com>
2023-03-11 23:05:11 +0000

    Update PHS_NASSS_Cardiac_WebPlotDigitizerSaveData.R
    Amended data frame and filenames used to include "2023" tag. Added 2022 summary, and removed 2023 summary data too once done.

--- PHS_NASSS_Cardiac_WebPlotDigitizerSaveData.R
+++ PHS_NASSS_Cardiac_WebPlotDigitizerSaveData.R
@@ -7,11 +7,11 @@ setwd(DataDirectory)
 
 # As used in R with three columns, Date, Type and Value
 # "Long" format
-write.csv(cardiac.calls,file = "2019-2022 NASSS Cardiac calls (long).csv",
+write.csv(cardiac.calls,file = "2019-2023 NASSS Cardiac calls (long).csv",
           row.names = FALSE)
 
 # Spreadsheet-friendly format with three columns: Date, Baseline and Calls
-write.csv(cardiac.2019_2022.approx,file = "2019-2022 NASSS Cardiac calls (spreadsheet).csv",
+write.csv(cardiac.2019_2023.approx,file = "2019-2023 NASSS Cardiac calls (spreadsheet).csv",
           row.names = FALSE)
 
 # Output summary totals for each year.
@@ -22,22 +22,27 @@ write.csv(cardiac.2019_2022.approx,file = "2019-2022 NASSS Cardiac calls (spread
 #   filter(Date >= as.Date("2018-01-01") & Date <= as.Date("2018-12-31")) %>%
 #   summarise(Daily = sum(SevenDayAverage,na.rm = TRUE),Baseline = sum(Baseline))
 
-cardiac.2019.summary <- cardiac.2019_2022.approx %>%
+cardiac.2019.summary <- cardiac.2019_2023.approx %>%
   filter(Date >= as.Date("2019-01-01") & Date <= as.Date("2019-12-31")) %>%
   summarise(Daily = sum(SevenDayAverage,na.rm = TRUE),Baseline = sum(Baseline))
 
-cardiac.2020.summary <- cardiac.2019_2022.approx %>%
+cardiac.2020.summary <- cardiac.2019_2023.approx %>%
   filter(Date >= as.Date("2020-01-01") & Date <= as.Date("2020-12-31")) %>%
   summarise(Daily = sum(SevenDayAverage,na.rm = TRUE),Baseline = sum(Baseline))
 
-cardiac.2021.summary <- cardiac.2019_2022.approx %>%
+cardiac.2021.summary <- cardiac.2019_2023.approx %>%
   filter(Date >= as.Date("2021-01-01") & Date <= as.Date("2021-12-31")) %>%
+  summarise(Daily = sum(SevenDayAverage,na.rm = TRUE),Baseline = sum(Baseline))
+
+cardiac.2022.summary <- cardiac.2019_2023.approx %>%
+  filter(Date >= as.Date("2022-01-01") & Date <= as.Date("2022-12-31")) %>%
   summarise(Daily = sum(SevenDayAverage,na.rm = TRUE),Baseline = sum(Baseline))
 
 # Remove redundant data frames
 rm(cardiac.2020,cardiac.2020.7DA,cardiac.2020.baseline,cardiac.2020.long,
    cardiac.2021,cardiac.2021.7DA,cardiac.2021.baseline,cardiac.2021.long,
-   cardiac.2022,cardiac.2022.7DA,cardiac.2022.baseline,cardiac.2022.long)
+   cardiac.2022,cardiac.2022.7DA,cardiac.2022.baseline,cardiac.2022.long,
+   cardiac.2023.baseline,cardiac.2023.long)
 
 ####################
 setwd(RootDirectory)
