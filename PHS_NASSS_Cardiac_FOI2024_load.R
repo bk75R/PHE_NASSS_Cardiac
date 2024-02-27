e9c2a0e4bdd7af54f4cf14c39c51c97fb2c73eac
bk75R <82215025+bk75R@users.noreply.github.com>
2024-02-27 14:20:22 +0000

    Reformat Yrwk data
    Split Yrwk YYYY_ww data into Year and Week columns. This throws file errors but still works.

--- PHS_NASSS_Cardiac_FOI2024_load.R
+++ PHS_NASSS_Cardiac_FOI2024_load.R
@@ -6,6 +6,15 @@ FOI2024.data <- read_excel("FOI Aug 2023 WeeklyCardRespByAmbTrust.xlsx",
            range = "A3:K17",
            col_names = TRUE)
 
+FOI2024.data <- as_tibble(FOI2024.data)
 
+# Reformat date. Not as robust as splitting before"_", but it works.
+FOI2024.data.mod <- FOI2024.data %>%
+  mutate("Year" = as.numeric(substr(YrWk,1,4))) %>%
+  mutate("Week" = as.numeric(substr(YrWk,6,8))) %>%
+  select(-YrWk) %>%
+  relocate(Week, .before = EMAS) %>%
+  relocate(Year, .before = Week)
+  
 ####################
 setwd(RootDirectory)
