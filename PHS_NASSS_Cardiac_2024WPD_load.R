18bfd58a3261cd45709a889103aa4d629df22398
bk75R <82215025+bk75R@users.noreply.github.com>
2024-03-28 18:28:18 +0000

    Calculates scaled weekly calls
    Scales data between 0 and 1 and creates new data frame for scaled data.

--- PHS_NASSS_Cardiac_2024WPD_load.R
+++ PHS_NASSS_Cardiac_2024WPD_load.R
@@ -25,6 +25,10 @@ file.data.all <- data.frame(A = character(),
 )
 colnames(file.data.all) <- column.names
 
+column.names.normalised <- c("Date","CallsNormalised","Region","Report")
+file.data.all.scaled <- file.data.all # Data frame for scaled (0-1) dataset
+colnames(file.data.all.scaled) <- column.names.normalised
+
 for (Filecounter in 1:length(filenames)) {
   # Set up filename and region and report tags
   file.current <- filenames[Filecounter] # Get nth filename
@@ -43,13 +47,23 @@ for (Filecounter in 1:length(filenames)) {
   file.data <- cbind(file.data,Region,Report)
   colnames(file.data) <- column.names # Change column names
   
+  # Calculate normalised data
+  minCalls <- min(file.data$Calls,na.rm = TRUE)
+  maxCalls <- max(file.data$Calls,na.rm = TRUE)
+  
+  # Scaled data
+  file.data.scaled <- file.data %>%
+    mutate(Calls = (Calls - minCalls)/(maxCalls - minCalls))
+  colnames(file.data.scaled) <- column.names.normalised
+  
   # Add current file data to whole data frame
   file.data.all <- rbind.data.frame(file.data.all,file.data)
+  file.data.all.scaled <- rbind.data.frame(file.data.all.scaled,file.data.scaled)
   
 }
 rm(Filecounter,file.current,file.elements,file.report,file.region,Region,Report) # Delete temp loop variables
 file.data.all$Date <- as_date(file.data.all$Date) # Convert date formats
-
+file.data.all.scaled$Date <- as_date(file.data.all.scaled$Date) # Convert date formats
 
 ####################
 setwd(RootDirectory)
